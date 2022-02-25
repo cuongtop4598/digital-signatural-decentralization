3dccada7108acfdc61007a3a4de7cdfdb56d657b
cuongtop4598 <cuongtop4598@gmail.com>
2022-02-25 12:44:56 +0700

    contract address

--- cmd/contract_deploy.go
+++ cmd/contract_deploy.go
@@ -1,54 +1,54 @@
-// package main
+package main
 
-// import (
-// 	"context"
-// 	"digitalsignature/internal/app/service"
-// 	"digitalsignature/internal/app/service/document"
-// 	"fmt"
-// 	"log"
-// 	"math/big"
+import (
+	"context"
+	"digitalsignature/internal/app/service"
+	"digitalsignature/internal/app/service/document"
+	"fmt"
+	"log"
+	"math/big"
 
-// 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-// 	"github.com/ethereum/go-ethereum/ethclient"
-// )
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
-// const (
-// 	endpoint = "http://27.72.105.169:10198"
-// 	chainID  = int64(451998)
-// 	keystore = "../wallets/keystore/"
-// 	password = "123456"
-// )
+const (
+	endpoint = "http://27.72.105.169:10198"
+	chainID  = int64(451998)
+	keystore = "../wallets/keystore/"
+	password = "123456"
+)
 
-// func main() {
-// 	client, err := ethclient.Dial(endpoint)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	accountSrv := service.NewAccountService(keystore, password)
-// 	account, ks, err := accountSrv.GetAminAccount()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	auth, err := bind.NewKeyStoreTransactorWithChainID(ks, *account, big.NewInt(chainID))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fromAddress := account.Address
-// 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func main() {
+	client, err := ethclient.Dial(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	accountSrv := service.NewAccountService(keystore, password)
+	account, ks, err := accountSrv.GetAminAccount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth, err := bind.NewKeyStoreTransactorWithChainID(ks, *account, big.NewInt(chainID))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fromAddress := account.Address
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	auth.From = account.Address
-// 	auth.Nonce = big.NewInt(int64(nonce))
-// 	auth.Value = big.NewInt(0)
-// 	auth.GasLimit = uint64(0x47b760)
-// 	auth.GasPrice = big.NewInt(1000)
+	auth.From = account.Address
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(0x47b760)
+	auth.GasPrice = big.NewInt(1000)
 
-// 	contractAddress, _, _, err := document.DeployDocument(auth, client)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println("Contract address", contractAddress)
-// 	fmt.Println("Deploy done!")
-// }
+	contractAddress, _, _, err := document.DeployDocument(auth, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Contract address", contractAddress)
+	fmt.Println("Deploy done!")
+}
--- cmd/test_upload_file.go
+++ cmd/test_upload_file.go
@@ -1,54 +1,54 @@
-package main
+// package main
 
-import (
-	"bytes"
-	"io"
-	"log"
-	"mime/multipart"
-	"net/http"
-	"os"
-	"time"
+// import (
+// 	"bytes"
+// 	"io"
+// 	"log"
+// 	"mime/multipart"
+// 	"net/http"
+// 	"os"
+// 	"time"
 
-	"github.com/google/uuid"
-)
+// 	"github.com/google/uuid"
+// )
 
-func main() {
-	call("http://localhost:8081/v1/document/upload", "POST")
-}
+// func main() {
+// 	call("http://localhost:8081/v1/document/upload", "POST")
+// }
 
-func call(urlPath, method string) error {
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	os.Chdir(".")
+// func call(urlPath, method string) error {
+// 	client := &http.Client{
+// 		Timeout: time.Second * 10,
+// 	}
+// 	os.Chdir(".")
 
-	// New multipart writer.
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	fw, err := writer.CreateFormFile("doc", "test.pdf")
-	if err != nil {
-		return err
-	}
-	file, err := os.Open("../assets/test.pdf")
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(fw, file)
-	if err != nil {
-		return err
-	}
-	writer.Close()
-	req, err := http.NewRequest(method, urlPath+"?public=true&&user_id="+uuid.New().String(), bytes.NewReader(body.Bytes()))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	rsp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if rsp.StatusCode != http.StatusOK {
-		log.Printf("Request failed with response code: %d", rsp.StatusCode)
-	}
-	return nil
-}
+// 	// New multipart writer.
+// 	body := &bytes.Buffer{}
+// 	writer := multipart.NewWriter(body)
+// 	fw, err := writer.CreateFormFile("doc", "test.pdf")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	file, err := os.Open("../assets/test.pdf")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = io.Copy(fw, file)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	writer.Close()
+// 	req, err := http.NewRequest(method, urlPath+"?public=true&&user_id="+uuid.New().String(), bytes.NewReader(body.Bytes()))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Set("Content-Type", writer.FormDataContentType())
+// 	rsp, err := client.Do(req)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	if rsp.StatusCode != http.StatusOK {
+// 		log.Printf("Request failed with response code: %d", rsp.StatusCode)
+// 	}
+// 	return nil
+// }
