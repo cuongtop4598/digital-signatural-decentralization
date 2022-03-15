16f5fa29290f6bf01af7a21d75572da0a2bdf3e0
cuongtop4598 <cuongtop4598@gmail.com>
2022-03-15 08:30:30 +0700

    update contract

--- contracts/documents/Document.sol
+++ contracts/documents/Document.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "./IDC.sol";
 import "./utils/Context.sol";
 import "./StringsUtils.sol";
+
 /**
  * @dev Implementation of the {IDC} interface
  * 
@@ -17,18 +18,15 @@ import "./StringsUtils.sol";
         bytes32 infoHash;
         string phoneHash;
         string publicKey;
-        uint256 numDoc;
-        mapping(uint256 => Doc) docs;
+        uint documentSize;
+        mapping(uint => Doc) doc;
     }
-    struct UserList {
-        mapping(string => User) _uListPubKey;
-        mapping(string => User) _uListUID;
-        mapping(string => User) _uListPhoneHash;
-    }
+
+    User[] users;
+
     struct Doc {
         bytes signature;
     }
-    UserList _userlist;
 
     event IndexDocument(string publickey, uint256 numdoc, bytes signature);
     /**
@@ -36,16 +34,14 @@ import "./StringsUtils.sol";
      */
     function storeUser(string memory userID,string memory name,string memory cmnd, string memory dateOB, string memory phone,string memory gmail,string memory publicKey) 
     public override returns(bool) {
-        bytes32 hashif = hashUserInfo(name,cmnd,dateOB,phone,gmail);
-        
-        User storage u = _userlist._uListPubKey[publicKey];
-        
+        bytes32 hashInfo = hashUserInfo(name,cmnd,dateOB,phone,gmail);
+        uint256 idx = users.length;
+        users.push();
+        User storage u = users[idx];
         u.userID = userID;
-        u.infoHash = hashif;
+        u.infoHash = hashInfo;
         u.publicKey = publicKey;
         u.phoneHash = bytes32ToString(keccak256(abi.encodePacked(phone)));
-        u = _userlist._uListUID[userID];
-        u = _userlist._uListPhoneHash[u.phoneHash];
         return true;
     }
     /**
@@ -54,34 +50,59 @@ import "./StringsUtils.sol";
     function verifyUser(string memory name,string memory cmnd, string memory dateOB, string memory phone,string memory gmail,string memory publicKey)
     public override view returns(bool) {
          bytes32 hashInfo = hashUserInfo(name,cmnd,dateOB,phone,gmail);
-         if (compareBytes(_userlist._uListPubKey[publicKey].infoHash, hashInfo)){
-             return true;
-         } else {
-             return false;
+         uint i = 0;
+         for (i = 0;  i < users.length; i++ ) {
+            if(compareBytes(users[i].infoHash, hashInfo)){
+                return true;
+            }
          }
-    }                            
+         return false;
+    }       
+
+                       
     /**
      * @dev Save Document to Chain
      * 
      * Return index of Document in the document list of the owner
      */
     function saveDoc(string memory phone,bytes memory signature) public override returns (uint256) {
-        string memory hashphone = bytes32ToString(keccak256(abi.encodePacked(phone)));
-        uint256 numDoc = _userlist._uListPhoneHash[hashphone].numDoc;
-         _userlist._uListPhoneHash[hashphone].numDoc = numDoc + 1;
-        Doc storage doc = _userlist._uListPhoneHash[hashphone].docs[numDoc];
-        doc.signature = signature;
-        emit IndexDocument( _userlist._uListPhoneHash[hashphone].publicKey ,numDoc,signature);
-        return  _userlist._uListPhoneHash[hashphone].numDoc;
+        string memory hashPhone = bytes32ToString(keccak256(abi.encodePacked(phone)));
+        uint index;
+        for (uint i = 0;  i < users.length; i++ ) {
+            if(keccak256(abi.encodePacked(users[i].phoneHash)) == keccak256(abi.encodePacked(hashPhone))){
+                index = i;
+            }
+        }
+        users[index].doc[users[index].documentSize].signature = signature;
+        users[index].documentSize = users[index].documentSize + 1;
+        emit IndexDocument( users[index].publicKey ,users[index].documentSize - 1, users[index].doc[users[index].documentSize-1].signature);
+        return  users[index].documentSize-1;
     }
 
     /**
      * @dev Return True if Doc wasn't change else False
      */
     function verifyDoc(string memory phone, bytes32 digest, uint indexDoc) public view override returns(bool) {
-        User storage u = _userlist._uListPhoneHash[phone];
-        Doc memory d = u.docs[indexDoc];
-        return keccak256(abi.encodePacked(recoverSigner(digest,d.signature))) == keccak256(abi.encodePacked(u.publicKey));
+        string memory hashPhone = bytes32ToString(keccak256(abi.encodePacked(phone)));
+        uint index;
+        for (uint i = 0;  i < users.length; i++ ) {
+            if(keccak256(abi.encodePacked(users[i].phoneHash)) == keccak256(abi.encodePacked(hashPhone))){
+                index = i;
+            }
+        }
+        return keccak256(abi.encodePacked(recoverSigner(digest,users[index].doc[indexDoc].signature))) == keccak256(abi.encodePacked(users[index].publicKey));
+    }
+    
+    function getSignature(string memory phone, uint indexDoc) public view returns (bytes memory) {
+        string memory hashPhone = bytes32ToString(keccak256(abi.encodePacked(phone)));
+       uint index;
+        for (uint i = 0;  i < users.length; i++ ) {
+            if(keccak256(abi.encodePacked(users[i].phoneHash)) == keccak256(abi.encodePacked(hashPhone))){
+                index = i;
+            }
+        }
+        Doc memory d = users[index].doc[indexDoc];
+        return d.signature;
     }
 
     /**
@@ -117,14 +138,6 @@ import "./StringsUtils.sol";
 
         // implicitly return (r, s, v)
     }
-
-    /**
-    * @dev Return hash UserInfo 
-    */
-    function GetHashUserInfo(string calldata userPubkey) public view returns (bytes32) {
-        bytes32 hashInfo = _userlist._uListPubKey[userPubkey].infoHash;
-        return hashInfo;
-    }  
     /**
      * @dev Return a hash of user information 
      */
