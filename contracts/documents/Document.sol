d99092e18d1d633833878ce9a2c3bc0c90dad8d9
cuongtop4598 <cuongtop4598@gmail.com>
2022-03-16 16:09:23 +0700

    update contract

--- contracts/documents/Document.sol
+++ contracts/documents/Document.sol
@@ -23,8 +23,7 @@ contract Document is Context, IDC {
         bytes signature;
     }
 
-    event IndexDocument(uint256 numdoc);
-    event Status(string);
+    event Number(string);
     /**
      * @dev Return True if user information is stored successfully, otherwise return False.
      */
@@ -74,12 +73,13 @@ contract Document is Context, IDC {
      */
     function saveDoc(string memory phone,bytes memory signature) public override {
         bytes32 phoneHash = hashPhoneNumber(phone);
-         uint i = 0;
+        uint i = 0;
         for ( i = 0;  i <= users.length; i++ ) {
            if(compareBytes(users[i].phoneHash, phoneHash)){
+                 string memory numDoc = uint2str(users[i].documentSize);
+                 emit Number(numDoc);
                  users[i].doc[users[i].documentSize].signature = signature;
                  users[i].documentSize = users[i].documentSize + 1;
-                 emit IndexDocument(users[i].documentSize - 1);
             }
         }
        revert("not found");
@@ -207,5 +207,26 @@ contract Document is Context, IDC {
         } else {
         return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
         }
+    }
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
